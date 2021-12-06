d598aee9c424aea7320babced211c9e2fd687923
Mike Carbone <mfcbone2@gmail.com>
2021-12-06 15:01:04 -0500

    :rocket: Adds session token logic

--- libs/models/User.js
+++ libs/models/User.js
@@ -16,10 +16,8 @@ class User {
 		} else {
 			// Default instantiation values
 			this.firstName = userDetails.firstName
-			this.stats = {
-				questionsAnswered: 0,
-				questionsSeen: 0,
-			}
+			this.stats = { questionsAnswered: 0, questionsSeen: 0 }
+
 			// Set constructed to true so we know not to reset these values
 			this.constructed = true
 			this.saveUser()
@@ -44,6 +42,39 @@ class User {
 
 	getSavedUser() {
 		return JSON.parse(localStorage.getItem(config.USER_KEY_LOCAL_STORAGE))
+	}
+
+	// The API says we should use a session token to avoid getting questions we've already answered
+	// Let's add a method that'll allow us to generate a session token and save it to our user
+	// This isnt super critical, so if it fails lets not error out, we can return an empty string
+	// https://opentdb.com/api_config.php
+	static async fetchSessionToken() {
+		try {
+			const res = await fetch(
+				'https://opentdb.com/api_token.php?command=request'
+			).then(r => r.json())
+
+			// If all is well, let's save the session token back to local storage
+			if (res.response_code === 0) {
+				this.sessionToken = res.token
+				this.saveUser()
+				return res.token
+			} else {
+				return ''
+			}
+		} catch (err) {
+			return ''
+		}
+	}
+
+	// A little memoization action to get a session token
+	// If we have a token, return it. If not, let's generate a new one
+	async getSessionToken() {
+		if (this.sessionToken) {
+			return this.sessionToken
+		}
+		const newToken = await this.fetchSessionToken()
+		return newToken
 	}
 }
 
